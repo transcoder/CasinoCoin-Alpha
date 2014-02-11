394a32e3afe3475c43e2ab1564d6a1502e6d3048
transcoder <transcoder264@gmail.com>
2014-02-11 02:06:58 -0700

    RPC port fix
    Fixed rpcport and testnet references

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 17950 : 47950;
+    return GetBoolArg("-testnet", false) ? 17970 : 47970;
 }
 
 Object JSONRPCError(int code, const string& message)
