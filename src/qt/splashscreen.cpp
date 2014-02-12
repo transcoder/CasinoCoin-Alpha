ed685a7c538e9d9360b632d25c6f2fdf302ff5ac
transcoder <transcoder264@gmail.com>
2014-02-12 04:00:45 -0700

    Update to wallet image and splash screen display

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -42,7 +42,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line3,versionText);
 
     // draw copyright stuff
-    pixPaint.setFont(QFont(font, 9*fontFactor));
+//    pixPaint.setFont(QFont(font, 9*fontFactor));
 //    pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line1,copyrightText1);
 //    pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line2,copyrightText2);
 
